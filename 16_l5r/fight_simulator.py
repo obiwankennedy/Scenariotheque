c9b4a2db5f975c5d20d19f50836a6523587e46f3
Renaud G <renaud@rolisteam.org>
2018-09-25 11:46:38 +0200

    Update fight_simulator.py
    Add comments

--- 16_l5r/fight_simulator.py
+++ 16_l5r/fight_simulator.py
@@ -4,10 +4,11 @@
 import subprocess
 import re
 
+# Les stats des deux personnages
 characterOne = {"name":"Ikoma Kae", "init":"8d10e10k4","attack":"9d10r1e10k4","dommage":"8d10e10k2+7","nd":25,"currentDommage":0,"hp":[15,21,27,33,39,45,51,57],"reduc":10,"void":4,"malus":[0,0,0,0,5,10,30],"currentMalus":0, "attackCount":2}
 characterTwo = {"name":"Akodo Eiichi", "init":"9d10e10k4+5","attack":"10d10r1e10k5","dommage":"8d10e10k2","nd":30,"currentDommage":0,"hp":[15,21,27,33,39,45,51,57],"reduc":3,"void":3,"malus":[0,0,2,7,12,17,37],"currentMalus":0, "attackCount":2}
 
-
+# Fonction pour lancer les dés
 def rollCommand(cmd):
   params = ['dice','-c',cmd]
   diceParser = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
@@ -16,10 +17,11 @@ def rollCommand(cmd):
   if m:
       return m.group(1)
 
+# fonction pour afficher un résultat d'un jet
 def printValue(characterName,rollName,rollScore):
     print("{} a obtenu {} à son jet de {}".format(characterName,rollScore,rollName))
 
-
+# Fonction de calcul du malus au dommage pour un personnage
 def computeMalus(character):
      dmg = character["currentDommage"]
      try:
@@ -30,23 +32,28 @@ def computeMalus(character):
 
 
 
-
+# Lance l'initiative
 init1 = rollCommand(characterOne["init"])
 init2 = rollCommand(characterTwo["init"])
 
+#Affiche les valeurs
 printValue(characterOne["name"],"initiative",init1)
 printValue(characterTwo["name"],"initiative",init2)
 
+#SI perso2 a une meilleure init alors perso2 devient perso1
 if init2 > init1:
     tree = characterOne
     characterOne = characterTwo
     characterTwo = tree
 
+#Tour de combats
 tour = 1
 while ( characterOne["currentDommage"] < max(characterOne["hp"]) and characterTwo["currentDommage"] < max(characterTwo["hp"])):
+    # Sauts de lignes et Affichage du numero du tour
     print("\n\n\nTour: "+str(tour))
     tour+=1
     usedVoid=False
+    #Attaques de perso1 sur perso2
     for i in range(0,characterOne["attackCount"]):
         finalOutput = "{} Attaque {} : {}"
         attack1 = rollCommand(characterOne["attack"])
@@ -69,6 +76,7 @@ while ( characterOne["currentDommage"] < max(characterOne["hp"]) and characterTw
 
 
     usedVoid=False
+    #Attaques perso2 sur perso1
     for i in range(0,characterTwo["attackCount"]):
         finalOutput = "{} Attaque {} : {}"
         attack1 = rollCommand(characterTwo["attack"])
@@ -92,13 +100,13 @@ while ( characterOne["currentDommage"] < max(characterOne["hp"]) and characterTw
     #print(characterOne)
     #print(characterTwo)
 
+# Affichage du gagnant
 if(characterOne["currentDommage"]>= max(characterOne["hp"])):
     print("Le gagnant est: {}".format(characterTwo["name"]))
-
 else:
     print("Le gagnant est: {}".format(characterOne["name"]))
 
-
+# Affichage des valeurs des personnages, permets de voir les dommanges encaissés.
 print("\n\n\n\n\n\n\n")
 print(characterTwo)
 print(characterOne)
